0493bda0bcc29dd5861ba5edc96719cd57ab4481
H0RAC3Z <horace-zeng@outlook.com>
2026-04-24 15:52:09 -0400

    working page turning

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function loadPage(pageID) {
     document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
-    document.getElementById(pageID).classList("active");
+    document.getElementById(pageID).classList.add("active");
 }
